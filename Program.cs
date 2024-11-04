adf4c3b90b3cbc13111ec035237b05e3c252378e
Ben Bluemel <bbluemel@jhacorp.com>
2024-11-04 08:27:07 -0700

    Add health check file

--- Program.cs
+++ Program.cs
@@ -2,66 +2,67 @@
 using System.Net;
 using System.Threading;
 
-class Program
+HttpListener listener = new HttpListener();
+listener.Prefixes.Add("http://*:80/");
+listener.Start();
+Console.WriteLine("Listening...");
+
+while (true)
 {
-	static void Main()
+    HttpListenerContext context = listener.GetContext();
+    HttpListenerRequest request = context.Request;
+    HttpListenerResponse response = context.Response;
+
+    switch (request.Url.AbsolutePath)
+    {
+        case "/health":
+            HealthHandler(response);
+            break;
+        case "/testtimeout":
+            TestTimeoutHandler(response);
+            break;
+        case "/testalmost":
+            TestAlmostHandler(response);
+            break;
+        case "/test":
+            TestHandler(response);
+            break;
+        default:
+            response.StatusCode = (int)HttpStatusCode.NotFound;
+            response.Close();
+            break;
+    }
+}
+
+void HealthHandler(HttpListenerResponse response)
+{
+    if (File.Exists("stop.txt"))
 	{
-		HttpListener listener = new HttpListener();
-		listener.Prefixes.Add("http://*:80/");
-		listener.Start();
-		Console.WriteLine("Listening...");
-
-		while (true)
-		{
-			HttpListenerContext context = listener.GetContext();
-			HttpListenerRequest request = context.Request;
-			HttpListenerResponse response = context.Response;
-
-			switch (request.Url.AbsolutePath)
-			{
-				case "/health":
-					HealthHandler(response);
-					break;
-				case "/testtimeout":
-					TestTimeoutHandler(response);
-					break;
-				case "/testalmost":
-					TestAlmostHandler(response);
-					break;
-				case "/test":
-					TestHandler(response);
-					break;
-				default:
-					response.StatusCode = (int)HttpStatusCode.NotFound;
-					response.Close();
-					break;
-			}
-		}
+		response.StatusCode = (int)HttpStatusCode.ServiceUnavailable;
 	}
-
-	static void HealthHandler(HttpListenerResponse response)
+	else
 	{
 		response.StatusCode = (int)HttpStatusCode.OK;
-		response.Close();
 	}
+	response.Close();
+}
 
-	static void TestTimeoutHandler(HttpListenerResponse response)
-	{
-		Thread.Sleep(40000);
-		response.StatusCode = (int)HttpStatusCode.OK;
-		response.Close();
-	}
+void TestTimeoutHandler(HttpListenerResponse response)
+{
+    Thread.Sleep(40000);
+    response.StatusCode = (int)HttpStatusCode.OK;
+    response.Close();
+}
 
-	static void TestAlmostHandler(HttpListenerResponse response)
-	{
-		Thread.Sleep(25000);
-		response.StatusCode = (int)HttpStatusCode.OK;
-		response.Close();
-	}
+void TestAlmostHandler(HttpListenerResponse response)
+{
+    Thread.Sleep(25000);
+    response.StatusCode = (int)HttpStatusCode.OK;
+    response.Close();
+}
 
-	static void TestHandler(HttpListenerResponse response)
-	{
-		response.StatusCode = (int)HttpStatusCode.OK;
-		response.Close();
-	}
+void TestHandler(HttpListenerResponse response)
+{
+    response.StatusCode = (int)HttpStatusCode.OK;
+    response.Close();
 }
